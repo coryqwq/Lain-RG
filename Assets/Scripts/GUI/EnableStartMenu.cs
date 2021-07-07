cd141b808fef8ba01965f4a2d22b59f4eebc1e25
unknown <cktran02@gmail.com>
2021-07-07 15:59:00 -0700

    scrolling background text

--- Assets/Scripts/GUI/EnableStartMenu.cs
+++ Assets/Scripts/GUI/EnableStartMenu.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 public class EnableStartMenu : MonoBehaviour
 {
     public GameObject startMenu;
+    public GameObject firstButton;
+    bool flag = false;
     // Update is called once per frame
     void Update()
     {
-        if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("BootCompoundStartMenu"))
+        if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("BootCompoundStartMenu") && flag == false)
         {
             startMenu.SetActive(true);
+            FindObjectOfType<EventSystem>().SetSelectedGameObject(firstButton);
+            flag = true;
         }
     }
 }
--- Assets/Scripts/GUI/ScrollingBackground.cs
+++ Assets/Scripts/GUI/ScrollingBackground.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class ScrollingBackground : MonoBehaviour
 {
-    
+    public int speed;
+    public float distance;
+    public float maxDistance;
+    Vector3 startPos;
+    Quaternion startAngle;
+
+    bool flag = false;
+    private void Start()
+    {
+        startPos = transform.position;
+        startAngle = transform.rotation;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.up * Time.deltaTime * speed, transform);
+       
+        if (Mathf.Abs(startPos.x - transform.position.x) >= distance && flag == false)
+        {
+            Instantiate(gameObject, startPos, startAngle);
+            flag = true;
+        }
+        if (Mathf.Abs(startPos.x - transform.position.x) >= maxDistance)
+        {
+            GameObject.Destroy(gameObject);
+        }
     }
 }
