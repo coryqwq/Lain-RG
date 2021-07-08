d087a69ee2d2e0fcd30a2b26276c6cd739542ab6
unknown <cktran02@gmail.com>
2021-07-07 22:03:46 -0700

    level select gui

--- Assets/Scripts/GUI/ButtonFX.cs
+++ Assets/Scripts/GUI/ButtonFX.cs
@@ -1,26 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Video;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 public class ButtonFX : MonoBehaviour
 {
     public Animator buttonAnim;
-    public void AuthorizeUserOn()
+
+    public VideoClip[] preview;
+    public VideoPlayer videoPlayer;
+    public string[] buttonName;
+    public GameObject video;
+
+    public Text title;
+    public Text artist;
+    public string[] songTitle;
+    public string[] songArtist;
+    public void Highlight()
     {
+        if (FindObjectOfType<EventSystem>().currentSelectedGameObject != gameObject)
+        {
+            FindObjectOfType<EventSystem>().SetSelectedGameObject(gameObject);
+        }
         buttonAnim.SetBool("select", true);
     }
-    public void AuthorizeUserOff()
-    {
-        buttonAnim.SetBool("select", false);
-    }
-    public void LoadDataOn()
-    {
-        buttonAnim.SetBool("select", true);
-    }
-    public void LoadDataOff()
+
+    public void Unhighlight()
     {
         buttonAnim.SetBool("select", false);
     }
 
+    public void PlayPreview()
+    {
+        video.SetActive(true);
+        for (int i = 0; i < preview.Length; i++)
+        {
+            if (gameObject.name == buttonName[i])
+            {
+                title.text = songTitle[i];
+                artist.text = songArtist[i];
 
+                videoPlayer.Stop();
+                videoPlayer.clip = preview[i];
+                videoPlayer.Play();
+            }
+        }
+    }
+
+    public void DisplayNull()
+    {
+        title.text = "NULL";
+        artist.text = "NULL";
+
+        videoPlayer.Stop();
+        video.SetActive(false);
+    }
 }
